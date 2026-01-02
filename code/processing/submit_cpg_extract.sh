a6e7799c44e5605344a93bf10c42e290c0a8da7f
Toby Baker <tobybaker@mednet.ucla.edu>
2026-01-01 23:42:30 -0800

    updated submission script for sample id

--- code/processing/submit_cpg_extract.sh
+++ code/processing/submit_cpg_extract.sh
@@ -6,5 +6,5 @@
 #SBATCH --time=24:00:00
 #SBATCH --exclusive
 
-# Usage: sbatch submit_cpg_extraction.sh <bam_file> <output_dir>
-~/miniconda3/envs/pytorch_env/bin/python /hot/user/tobybaker/ROCIT_Paper/code/processing/extract_cpg_info_from_bam.py "$1" "$2" --workers 8
+# Usage: sbatch submit_cpg_extraction.sh <bam_file> <output_dir> <sample_id>
+~/miniconda3/envs/pytorch_env/bin/python /hot/user/tobybaker/ROCIT_Paper/code/processing/extract_cpg_info_from_bam.py "$1" "$2" "$3" --workers 8
